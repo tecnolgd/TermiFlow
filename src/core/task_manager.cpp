4be64d5cf0756304dba4880e35679d486b17db5e
Tanay Kumar <ktanay1704@gmail.com>
2026-05-04 23:11:28 +0530

    Delete src/core/task_manager.cpp

--- src/core/task_manager.cpp
+++
@@ -1,6 +0,0 @@
-// Will be implemented in the upcoming versions.
-#include "../../include/task_manager.hpp"
-
-void taskManager(){
-    std::cout<<">> Not available for the current version. <<\n";
-}
